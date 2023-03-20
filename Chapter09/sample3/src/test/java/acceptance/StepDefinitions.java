447e9fad8e572df9d2369092c3369b364765adbd
anandka2000 <37380705+anandka2000@users.noreply.github.com>
2023-03-19 21:57:25 -0400

    Update StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -32,6 +32,7 @@ public class StepDefinitions {
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
+    }
         
     @When("^the calculator div them$")
     public void the_calculator_div_them() throws Throwable {
